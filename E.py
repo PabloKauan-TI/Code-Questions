5e8112cfb32abae0d7071d1db22eb6e9829aa4eb
PabloKauan-TI <kpablo429@gmail.com>
2024-08-03 14:51:31 -0300

    .

--- E.py
+++ E.py
@@ -7,10 +7,8 @@ N, K = (int(number) for number in entrada1.split(' '))
 
 #verifica os mesmos
 if not 1<=N<=1000000:
-    print('Valor fora do intervalo permitido')
     exit()
 if not 1<=K<=1000000000:
-    print('Valor fora do intervalo permitido')
     exit()
         
 #recebe flores transformando em um vetor inteiro
@@ -20,14 +18,11 @@ for i in range(N):
 
 #verifica se foram listadas todas as alturas
 if not len(P)==N:
-    print('Não foi listado todos os brinquedos')
     exit()
 
 flor = 0
 for i in range(K):  
     P.sort(reverse=True)  
-    if P[flor]==0 and flor<(N-1):
-        flor+=1
     if P[flor]==0 and flor==(N-1):
         print(0)
         exit()
