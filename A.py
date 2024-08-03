1af1ccd351cd1dfc26d8434f71d480be216f7195
PabloKauan-TI <kpablo429@gmail.com>
2024-08-03 16:18:25 -0300

    SBC 2023 - Question A (Completed)

--- A.py
+++ A.py
@@ -1,29 +1,24 @@
-while True:
-    try:
-        #recebe os dois primeiros parametros e os guarda
-        entrada1 = input()
-        N, H = (int(number) for number in entrada1.split(' '))
+#recebe os dois primeiros parametros e os guarda
+entrada1 = input()
+N, H = (int(number) for number in entrada1.split(' '))
         
-        #verifica os mesmos
-        if not 1<=N<=6:
-            raise ValueError('Valor fora do intervalo permitido')
-        if not 90<=H<=200:
-            raise ValueError('Valor fora do intervalo permitido')
+#verifica os mesmos
+if not 1<=N<=6:
+    exit()
+if not 90<=H<=200:
+    exit()
 
-        #recebe as alturas dos brinquedos e cria um vetor de strings
-        B = input().split(' ') 
+#recebe as alturas dos brinquedos e cria um vetor de strings
+B = input().split(' ') 
 
-        #verifica se foram listadas todas as alturas
-        if not len(B)==N:
-            raise ValueError('Não foi listado todos os brinquedos')
+#verifica se foram listadas todas as alturas
+if not len(B)==N:
+    exit()
 
-        #Visualiza em quais brinquedos Joaozinho pode andar
-        R = 0
-        for valor in B:
-            if(H>=int(valor)):
-                R+=1
-        print(R)
-        break
-    
-    except ValueError as error:
-        print(error)
+#Visualiza em quais brinquedos Joaozinho pode andar
+R = 0
+for valor in B:
+    if(H>=int(valor)):
+        R+=1
+print(R)
+exit()
